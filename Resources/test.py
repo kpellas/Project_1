58035bb8ba227c40e467dd5d4ea0f8120f3a478b
hatimmansor <Hatimf2004@gmail.com>
2021-12-16 21:19:43 +1100

    Update test.py

--- Resources/test.py
+++ Resources/test.py
@@ -3,3 +3,7 @@
 # Not every CSV requires an encoding, but be aware this can come up
 raw_data = pd.read_csv(file_one, encoding="ISO-8859-1",low_memory=False)
 raw_data.head()
+
+
+data_state=raw_data.groupby('State')["Crash ID"].unique().count()
+print(data_state)
