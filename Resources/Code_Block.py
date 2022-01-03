d6685c0b8d6595bd0e92e2482a5a9391284a4bef
Kelly Pellas <kkpellas@gmail.com>
2022-01-03 16:48:56 +1100

    corrected issue with images

--- Resources/Code_Block.py
+++
@@ -1,9 +0,0 @@
-
-# Read our Data file with the pandas library
-# Not every CSV requires an encoding, but be aware this can come up
-raw_data = pd.read_csv(file_one, encoding="ISO-8859-1",low_memory=False)
-raw_data.head()
-
-
-data_state=raw_data.groupby('State')["Crash ID"].unique().count()
-print(data_state)
